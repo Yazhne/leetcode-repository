8478ad5c2bc6ecd77b60dc3baa86382e35dcae55
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-12 22:18:54 +0530

    Create 2025-RedistributeCharactersToMakeAllStringsEqual

---
+++ 2025-RedistributeCharactersToMakeAllStringsEqual/2025-RedistributeCharactersToMakeAllStringsEqual.java
@@ -0,0 +1,28 @@
+// Last updated: 8/12/2026, 10:19:03 PM
+class Solution {
+    public boolean makeEqual(String[] words) {
+      if (words.length == 1) {
+			return true;
+		}
+		int totalCharCount = 0;
+		for (String s : words) {
+			totalCharCount += s.length();
+		}
+		if (totalCharCount % words.length != 0) {
+			return false;
+		}
+
+		int[] myMap = new int[26];
+		for (String s : words) {
+			for (char c : s.toCharArray()) {
+				myMap[c - 'a']++;
+			}
+		}
+		for (int i : myMap) {
+			if (i % words.length != 0) {
+				return false;
+			}
+		}
+		return true; 
+    }
+}
