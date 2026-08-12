e3e8a4cb3af3862e082ac79a71e7d7d3a217e323
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-12 22:24:22 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/263-UglyNumber.java
@@ -0,0 +1,18 @@
+// Last updated: 8/12/2026, 10:24:30 PM
+class Solution {
+    public boolean isUgly(int n) {
+        if(n<=0){
+            return false;
+        }
+        while(n%3==0){
+            n=n/3;
+        }
+        while(n%2==0){
+            n=n/2;
+        }
+        while(n%5==0){
+            n=n/5;
+        }
+        return n==1;
+    }
+}
