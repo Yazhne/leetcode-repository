a7e14f7170bb8d5e3140582c8435e5510bc65afb
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-12 22:17:30 +0530

    Create 4338-AggregateTwoTimeSeries

---
+++ 4338-AggregateTwoTimeSeries/4338-AggregateTwoTimeSeries.py
@@ -0,0 +1,28 @@
+# Last updated: 8/12/2026, 10:17:38 PM
+class Solution:
+    def aggregateTimeSeries(self, series1: list[list[int]], series2: list[list[int]]) -> list[list[int]]:
+        i=j=0
+        ans=[]
+        while i<len(series1) or j<len(series2):
+            if j==len(series2) or (i<len(series1) and series1[i][0]<series2[j][0]):
+                t=series1[i][0]
+            elif i==len(series1) or series2[j][0]<series1[i][0]:
+                t=series2[j][0]
+            else:
+                t=series1[i][0]
+            if i<len(series1) and series1[i][0]==t:
+                v1=series1[i][1]
+                i+=1
+            elif i<len(series1):
+                v1=series1[i][1]
+            else:
+                v1=0
+            if j<len(series2) and series2[j][0]==t:
+                v2=series2[j][1]
+                j+=1
+            elif j<len(series2):
+                v2=series2[j][1]
+            else:
+                v2=0
+            ans.append([t,v1+v2])
+        return ans
