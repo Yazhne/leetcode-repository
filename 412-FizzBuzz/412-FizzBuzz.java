4a90f24e046604f5726c9c722de5cffb2d586b1b
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-12 22:23:21 +0530

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.java
@@ -0,0 +1,22 @@
+// Last updated: 8/12/2026, 10:23:29 PM
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List <String> a = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5==0){
+                a.add("FizzBuzz");
+            }
+            else if(i%3==0){
+                a.add("Fizz");
+            }
+            else if(i%5==0){
+                a.add("Buzz");
+            }
+            else{
+                a.add(String.valueOf(i));
+            }
+        }
+        return a;
+        
+    }
+}
