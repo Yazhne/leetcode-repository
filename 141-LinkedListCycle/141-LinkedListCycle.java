703048724a36cf5f3afd89d6f151df0f30dd79ea
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-12 22:26:22 +0530

    Create 141-LinkedListCycle

---
+++ 141-LinkedListCycle/141-LinkedListCycle.java
@@ -0,0 +1,18 @@
+// Last updated: 8/12/2026, 10:26:31 PM
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;        
+    }
+}
