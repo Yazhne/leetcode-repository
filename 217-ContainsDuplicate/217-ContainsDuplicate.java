873c0b452311be82444a726e1aa20a6727955765
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-12 22:25:01 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -0,0 +1,13 @@
+// Last updated: 8/12/2026, 10:25:10 PM
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
