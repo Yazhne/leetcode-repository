79422ac3e326f5289ab3491638d9c5ff39f9fb52
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-12 22:19:27 +0530

    Create 1542-ConsecutiveCharacters

---
+++ 1542-ConsecutiveCharacters/1542-ConsecutiveCharacters.java
@@ -0,0 +1,23 @@
+// Last updated: 8/12/2026, 10:19:35 PM
+class Solution {
+    public int maxPower(String s) {
+        int maxiCount = 0;
+        int count = 0;
+        char currentChar = ' ';
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == currentChar) {
+                count++;
+            } else {
+                count = 1;
+                currentChar = ch;
+            }
+
+            maxiCount = Math.max(count, maxiCount);
+        }
+
+        return maxiCount;
+    }
+}
