c8956804d0aa9ae38b9142d737a96e58f6befb72
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-12 22:22:34 +0530

    Create 520-DetectCapital

---
+++ 520-DetectCapital/520-DetectCapital.java
@@ -0,0 +1,9 @@
+// Last updated: 8/12/2026, 10:22:42 PM
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(word.equals(word.toUpperCase()) || word.equals(word.toLowerCase()) || word.equals(word.substring(0, 1).toUpperCase()+word.substring(1).toLowerCase())){
+            return true;
+        }
+        return false;
+    }
+}
