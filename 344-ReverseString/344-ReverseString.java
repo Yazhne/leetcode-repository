8b6ea95bf5c58570ece59919c7c07893e7d49f8e
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-12 22:23:52 +0530

    Create 344-ReverseString

---
+++ 344-ReverseString/344-ReverseString.java
@@ -0,0 +1,15 @@
+// Last updated: 8/12/2026, 10:24:00 PM
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right =s.length-1;
+        while(left<right){
+            char temp = s[left];
+            s[left]=s[right];
+            s[right]=temp;
+            left++;
+            right--;
+        }
+        
+    }
+}
