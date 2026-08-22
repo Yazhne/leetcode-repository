7f6d20ace200d07fcfc4ae1636a9d41cfe71435c
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-22 15:06:51 +0530

    File updated at 8/22/2026, 3:07:02 PM

--- 1341-MovieRating/1341-MovieRating.java
+++ 1341-MovieRating/1341-MovieRating.java
@@ -1,25 +1,24 @@
-// Last updated: 8/22/2026, 3:05:31 PM
+// Last updated: 8/22/2026, 3:07:02 PM
 1class Solution {
-2    public int[][] merge(int[][] intervals) {
-3        // Sort intervals based on start time
-4        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-5
-6        List<int[]> merged = new ArrayList<>();
-7        int[] prev = intervals[0];
-8
-9        for (int i = 1; i < intervals.length; i++) {
-10            // Overlap condition: current start <= previous end
-11            if (intervals[i][0] <= prev[1]) {
-12                // Merge intervals
-13                prev[1] = Math.max(prev[1], intervals[i][1]);
-14            } else {
-15                // No overlap, add previous interval to result
-16                merged.add(prev);
-17                prev = intervals[i];
+2    public int lengthOfLongestSubstring(String s) {
+3        int n = s.length();
+4        int maxLength = 0;
+5        Set<Character> charSet = new HashSet<>();
+6        int left = 0;
+7        
+8        for (int right = 0; right < n; right++) {
+9            if (!charSet.contains(s.charAt(right))) {
+10                charSet.add(s.charAt(right));
+11                maxLength = Math.max(maxLength, right - left + 1);
+12            } else {
+13                while (charSet.contains(s.charAt(right))) {
+14                    charSet.remove(s.charAt(left));
+15                    left++;
+16                }
+17                charSet.add(s.charAt(right));
 18            }
 19        }
-20
-21        merged.add(prev); // Add the last interval
-22        return merged.toArray(new int[merged.size()][]);
-23    }
-24}
+20        
+21        return maxLength;
+22    }
+23}
