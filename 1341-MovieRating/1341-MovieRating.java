ada7dd339ebf229b7d05bd488c63f15c0bc7c99e
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-22 14:30:33 +0530

    File updated at 8/22/2026, 2:30:44 PM

--- 1341-MovieRating/1341-MovieRating.java
+++ 1341-MovieRating/1341-MovieRating.java
@@ -1,15 +1,32 @@
-// Last updated: 8/22/2026, 2:29:25 PM
+// Last updated: 8/22/2026, 2:30:44 PM
 1class Solution {
-2    public int minMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f) {
-3        if(a == e || b == f) {
-4            if(a == e && a == c && ((d-b) * (d-f) < 0)) return 2;
-5            if(b == f && b == d && ((c-a) * (c-e) < 0)) return 2;
-6            return 1;
-7        }
-8        if(Math.abs(c - e) == Math.abs(d - f)) {
-9            if(Math.abs(c - a) == Math.abs(d - b) && Math.abs(e - a) == Math.abs(f - b) && ((b-f) * (b-d) < 0))  return 2;
-10            return 1;
-11        }
-12        return 2;
-13    }
-14}
+2    public List<Integer> spiralOrder(int[][] matrix) {
+3        int top = 0, bottom = matrix.length - 1;
+4        int left = 0, right = matrix[0].length - 1;
+5        List<Integer> spiral = new ArrayList<>();
+6
+7        while (top <= bottom && left <= right) {
+8            for (int i = left; i <= right; i++)
+9                spiral.add(matrix[top][i]);
+10            top++;
+11
+12            for (int j = top; j <= bottom; j++)
+13                spiral.add(matrix[j][right]);
+14            right--;
+15
+16            if (top <= bottom) {
+17                for (int k = right; k >= left; k--)
+18                    spiral.add(matrix[bottom][k]);
+19                bottom--;
+20            }
+21
+22            if (left <= right) {
+23                for (int l = bottom; l >= top; l--)
+24                    spiral.add(matrix[l][left]);
+25                left++;
+26            }
+27        }
+28
+29        return spiral;
+30    }
+31}
