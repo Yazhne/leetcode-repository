7857bf3d2b875d5ddd3bb53d6dc7eed72f836cb5
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-22 14:53:29 +0530

    File updated at 8/22/2026, 2:53:40 PM

--- 1341-MovieRating/1341-MovieRating.java
+++ 1341-MovieRating/1341-MovieRating.java
@@ -1,27 +1,25 @@
-// Last updated: 8/22/2026, 2:49:00 PM
+// Last updated: 8/22/2026, 2:53:40 PM
 1class Solution {
-2    public int trap(int[] height) {
-3        int n = height.length;
-4        int left = 0, right = n-1;
-5        int leftMax = 0, rightMax = 0;
-6        int res = 0;
-7        while (left <= right) {
-8            if (height[left] <= height[right]) {
-9                if (height[left] >= leftMax) {
-10                    leftMax = height[left];
-11                } else {
-12                    res += leftMax - height[left];
-13                }
-14                left++;
-15            } else {
-16                if (height[right] >= rightMax) {
-17                    rightMax = height[right];
-18                } else {
-19                    res += rightMax - height[right];
-20                }
-21                right--;
-22            }
-23        }
-24        return res;
-25    }
-26}
+2    public int findDuplicate(int[] nums) {
+3        int slow = nums[0];
+4        int fast = nums[0];
+5
+6        while (true) {
+7            slow = nums[slow];
+8            fast = nums[nums[fast]];
+9
+10            if (slow == fast) {
+11                break;
+12            }
+13        }
+14
+15        int slow2 = nums[0];
+16
+17        while (slow != slow2) {
+18            slow = nums[slow];
+19            slow2 = nums[slow2];
+20        }
+21
+22        return slow;        
+23    }
+24}
