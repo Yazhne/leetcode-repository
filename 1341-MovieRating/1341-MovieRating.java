7aab1b0b97b9fcc386c569bc86b5044a076e1109
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-22 15:05:20 +0530

    File updated at 8/22/2026, 3:05:31 PM

--- 1341-MovieRating/1341-MovieRating.java
+++ 1341-MovieRating/1341-MovieRating.java
@@ -1,18 +1,25 @@
-// Last updated: 8/22/2026, 3:03:50 PM
-1public class Solution {
-2    public int maxProduct(int[] A) {
-3        if (A == null || A.length == 0) {
-4            return 0;
-5        }
-6        int max = A[0], min = A[0], result = A[0];
-7        for (int i = 1; i < A.length; i++) {
-8            int temp = max;
-9            max = Math.max(Math.max(max * A[i], min * A[i]), A[i]);
-10            min = Math.min(Math.min(temp * A[i], min * A[i]), A[i]);
-11            if (max > result) {
-12                result = max;
-13            }
-14        }
-15        return result;
-16    }
-17}
+// Last updated: 8/22/2026, 3:05:31 PM
+1class Solution {
+2    public int[][] merge(int[][] intervals) {
+3        // Sort intervals based on start time
+4        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+5
+6        List<int[]> merged = new ArrayList<>();
+7        int[] prev = intervals[0];
+8
+9        for (int i = 1; i < intervals.length; i++) {
+10            // Overlap condition: current start <= previous end
+11            if (intervals[i][0] <= prev[1]) {
+12                // Merge intervals
+13                prev[1] = Math.max(prev[1], intervals[i][1]);
+14            } else {
+15                // No overlap, add previous interval to result
+16                merged.add(prev);
+17                prev = intervals[i];
+18            }
+19        }
+20
+21        merged.add(prev); // Add the last interval
+22        return merged.toArray(new int[merged.size()][]);
+23    }
+24}
