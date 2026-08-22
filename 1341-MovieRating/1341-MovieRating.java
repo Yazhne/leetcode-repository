706b605e9fd102e6a3b6d8aa7efd2665a06eb00d
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-22 14:21:44 +0530

    Create 1341-MovieRating

---
+++ 1341-MovieRating/1341-MovieRating.java
@@ -0,0 +1,14 @@
+// Last updated: 8/22/2026, 2:21:55 PM
+1class Solution {
+2    public boolean hasAlternatingBits(int n) {
+3        String binary = Integer.toBinaryString(n);
+4        char[] arr = binary.toCharArray();
+5
+6        for (int i = 1; i < arr.length; i++) {
+7            if (arr[i] == arr[i - 1]) {
+8                return false;
+9            }
+10        }
+11        return true;
+12    }
+13}
