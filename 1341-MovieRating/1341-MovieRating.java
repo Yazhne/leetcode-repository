c68077fb4ca55f163064e6eee25744074cdb03d2
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-22 14:22:34 +0530

    File updated at 8/22/2026, 2:22:45 PM

--- 1341-MovieRating/1341-MovieRating.java
+++ 1341-MovieRating/1341-MovieRating.java
@@ -1,14 +1,26 @@
-// Last updated: 8/22/2026, 2:21:55 PM
+// Last updated: 8/22/2026, 2:22:45 PM
 1class Solution {
-2    public boolean hasAlternatingBits(int n) {
-3        String binary = Integer.toBinaryString(n);
-4        char[] arr = binary.toCharArray();
-5
-6        for (int i = 1; i < arr.length; i++) {
-7            if (arr[i] == arr[i - 1]) {
-8                return false;
-9            }
-10        }
-11        return true;
-12    }
-13}
+2    public long maximumSubarraySum(int[] nums, int k) {
+3        int n = nums.length;
+4        Map<Integer,Long> map = new HashMap<>();
+5        long prefixsum=0;
+6        long maxSum=Long.MIN_VALUE;
+7        for(int i=0;i<n;i++)
+8        {
+9            if(map.getOrDefault(nums[i],Long.MAX_VALUE)>prefixsum)
+10            {
+11                 map.put(nums[i],prefixsum);
+12            }
+13            prefixsum+=nums[i];
+14            if(map.containsKey(nums[i]-k))
+15            {
+16                 maxSum=Math.max(maxSum,prefixsum-map.get(nums[i]-k));
+17            }
+18            if(map.containsKey(nums[i]+k))
+19            {
+20                 maxSum=Math.max(maxSum,prefixsum-map.get(nums[i]+k));
+21            }
+22        }
+23        return maxSum==Long.MIN_VALUE ? 0 : maxSum;
+24    }
+25}
