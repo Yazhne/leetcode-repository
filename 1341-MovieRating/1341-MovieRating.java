fbfcf4c0212d694f3ebde4602fe6d623e154c229
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-22 14:26:10 +0530

    File updated at 8/22/2026, 2:26:21 PM

--- 1341-MovieRating/1341-MovieRating.java
+++ 1341-MovieRating/1341-MovieRating.java
@@ -1,32 +1,39 @@
-// Last updated: 8/22/2026, 2:24:05 PM
+// Last updated: 8/22/2026, 2:26:21 PM
 1class Solution {
-2    public int maxTurbulenceSize(int[] arr) {
-3        int n = arr.length;
+2    public List<List<Integer>> findWinners(int[][] matches) {
+3        int[] losses = new int[100001];
 4
-5        if (n == 1) return 1;
-6
-7        int up = 1;
-8        int down = 1;
-9        int ans = 1;
-10
-11        for (int i = 1; i < n; i++) {
+5        for (int i = 0; i < matches.length; i++) {
+6            int win = matches[i][0];
+7            int loss = matches[i][1];
+8
+9            if (losses[win] == 0) {
+10                losses[win] = -1;
+11            } 
 12
-13            if (arr[i] > arr[i - 1]) {
-14                up = down + 1;
-15                down = 1;
-16            }
-17            else if (arr[i] < arr[i - 1]) {
-18                down = up + 1;
-19                up = 1;
-20            }
-21            else {
-22                up = 1;
-23                down = 1;
-24            }
-25
-26            ans = Math.max(ans, Math.max(up, down));
-27        }
-28
-29        return ans;
-30    }
-31}
+13            if (losses[loss] == -1) {
+14                losses[loss] = 1;
+15            } else {
+16                losses[loss]++;
+17            }
+18        }
+19
+20        List<Integer> zeroLoss = new ArrayList<>();
+21        List<Integer> oneLoss = new ArrayList<>();
+22
+23        List<List<Integer>> result = new ArrayList<>();
+24
+25        for (int i = 0; i < losses.length; i++) {
+26            if (losses[i] == -1) {
+27                zeroLoss.add(i);
+28            } else if (losses[i] == 1) {
+29                oneLoss.add(i);
+30            }
+31        }
+32
+33        result.add(zeroLoss);
+34        result.add(oneLoss);
+35
+36        return result;
+37    }
+38}
