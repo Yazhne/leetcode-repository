401a6349f9eac9ba63ddc721fb6304a3b9d0491c
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-22 14:58:08 +0530

    File updated at 8/22/2026, 2:58:20 PM

--- 1341-MovieRating/1341-MovieRating.java
+++ 1341-MovieRating/1341-MovieRating.java
@@ -1,26 +1,73 @@
-// Last updated: 8/22/2026, 2:55:19 PM
+// Last updated: 8/22/2026, 2:58:20 PM
 1class Solution {
-2    public int longestConsecutive(int[] nums) {
-3        Set<Integer> numSet = new HashSet<>();
-4
-5        for (int num : nums) {
-6            numSet.add(num);
-7        }
-8
-9        int longest = 0;
-10
-11        for (int n : numSet) {
-12            if (!numSet.contains(n - 1)) {
-13                int length = 1;
-14
-15                while (numSet.contains(n + length)) {
-16                    length++;
-17                }
+2
+3    private int minSubarraySum(int[] nums){
+4        int currSum = nums[0];
+5        int minSum = nums[0];
+6
+7        for(int i = 1; i < nums.length; i++){
+8            if(currSum > 0){
+9                currSum = 0;
+10            }
+11
+12            currSum += nums[i];
+13            minSum = Math.min(currSum, minSum);
+14        }
+15
+16        return minSum;
+17    }
 18
-19                longest = Math.max(longest, length);
-20            }
-21        }
+19    private int maxSubarraySum(int[] nums){
+20        int currSum = nums[0];
+21        int maxSum = nums[0];
 22
-23        return longest;
-24    }
-25}
+23        for(int i = 1; i < nums.length; i++){
+24            if(currSum < 0){
+25                currSum = 0;
+26            }
+27
+28            currSum += nums[i];
+29            maxSum = Math.max(currSum, maxSum);
+30        }
+31
+32        return maxSum;
+33    }
+34
+35    public int maxSubarraySumCircular(int[] nums) {
+36
+37        if(nums.length == 0){
+38            return 0;
+39        }
+40
+41        // Check if all elements are negative
+42        boolean x = true;
+43        int ans = Integer.MIN_VALUE;
+44
+45        for(int i = 0; i < nums.length; i++){
+46            if(nums[i] >= 0){
+47                x = false;
+48                break;
+49            }
+50
+51            ans = Math.max(ans, nums[i]);
+52        }
+53
+54        if(x){
+55            return ans;
+56        }
+57
+58        // Case 1: Maximum subarray does not wrap
+59        int ans1 = maxSubarraySum(nums);
+60
+61        // Case 2: Maximum subarray wraps around
+62        int arraySum = 0;
+63
+64        for(int i = 0; i < nums.length; i++){
+65            arraySum += nums[i];
+66        }
+67
+68        int ans2 = arraySum - minSubarraySum(nums);
+69
+70        return Math.max(ans1, ans2);
+71    }
+72}
