a1d02bbe4de454e2ab46fac858edc31c5828ffb4
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-22 15:08:23 +0530

    File updated at 8/22/2026, 3:08:35 PM

--- 1341-MovieRating/1341-MovieRating.java
+++ 1341-MovieRating/1341-MovieRating.java
@@ -1,24 +1,22 @@
-// Last updated: 8/22/2026, 3:07:02 PM
+// Last updated: 8/22/2026, 3:08:35 PM
 1class Solution {
-2    public int lengthOfLongestSubstring(String s) {
-3        int n = s.length();
-4        int maxLength = 0;
-5        Set<Character> charSet = new HashSet<>();
-6        int left = 0;
-7        
-8        for (int right = 0; right < n; right++) {
-9            if (!charSet.contains(s.charAt(right))) {
-10                charSet.add(s.charAt(right));
-11                maxLength = Math.max(maxLength, right - left + 1);
-12            } else {
-13                while (charSet.contains(s.charAt(right))) {
-14                    charSet.remove(s.charAt(left));
-15                    left++;
-16                }
-17                charSet.add(s.charAt(right));
-18            }
-19        }
-20        
-21        return maxLength;
-22    }
-23}
+2    public int[] maxSlidingWindow(int[] nums, int k) {
+3        Deque<Integer> q = new ArrayDeque<>();  // stores *indices*
+4        List<Integer> res = new ArrayList<>();
+5        for (int i = 0; i < nums.length; i++) {
+6            while (!q.isEmpty() && nums[q.getLast()] <= nums[i]) {
+7                q.removeLast();
+8            }
+9            q.addLast(i);
+10            // remove first element if it's outside the window
+11            if (q.getFirst() == i - k) {
+12                q.removeFirst();
+13            }
+14            // if window has k elements add to results (first k-1 windows have < k elements because we start from empty window and add 1 element each iteration)
+15            if (i >= k - 1) {
+16                res.add(nums[q.peek()]);
+17            }
+18        }
+19        return res.stream().mapToInt(i->i).toArray();            
+20    }
+21}
