430be539a75989b998873bd832e752ae53dfd200
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-22 14:23:53 +0530

    File updated at 8/22/2026, 2:24:05 PM

--- 1341-MovieRating/1341-MovieRating.java
+++ 1341-MovieRating/1341-MovieRating.java
@@ -1,20 +1,32 @@
-// Last updated: 8/22/2026, 2:23:39 PM
+// Last updated: 8/22/2026, 2:24:05 PM
 1class Solution {
-2    public int maximumSetSize(int[] nums1, int[] nums2) {
-3        Set<Integer> s1 = new HashSet(); 
-4        Set<Integer> s2 = new HashSet();
-5        Set<Integer> s3 = new HashSet(); 
-6        
-7        for (int num: nums1){
-8            s1.add(num);
-9            s3.add(num);
-10        } 
-11        for (int num: nums2) {
-12            s2.add(num);
-13            s3.add(num);
-14        } 
-15          
-16        return Math.min(Math.min(s1.size(), nums1.length/2) + 
-17                        Math.min(s2.size(), nums2.length/2), s3.size());                            
-18    }
-19}
+2    public int maxTurbulenceSize(int[] arr) {
+3        int n = arr.length;
+4
+5        if (n == 1) return 1;
+6
+7        int up = 1;
+8        int down = 1;
+9        int ans = 1;
+10
+11        for (int i = 1; i < n; i++) {
+12
+13            if (arr[i] > arr[i - 1]) {
+14                up = down + 1;
+15                down = 1;
+16            }
+17            else if (arr[i] < arr[i - 1]) {
+18                down = up + 1;
+19                up = 1;
+20            }
+21            else {
+22                up = 1;
+23                down = 1;
+24            }
+25
+26            ans = Math.max(ans, Math.max(up, down));
+27        }
+28
+29        return ans;
+30    }
+31}
