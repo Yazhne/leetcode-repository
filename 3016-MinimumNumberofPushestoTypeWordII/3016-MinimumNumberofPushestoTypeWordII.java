40001d05a7790e82c798bb98fad0b494fcf684b3
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-23 10:42:12 +0530

    File updated at 8/23/2026, 10:42:25 AM

--- 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
+++ 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
@@ -1,35 +1,16 @@
-// Last updated: 8/23/2026, 10:41:10 AM
+// Last updated: 8/23/2026, 10:42:25 AM
 1class Solution {
-2    public String RLE(String sequence) {
-3        StringBuilder sb = new StringBuilder();
-4        char[] nums = sequence.toCharArray();
-5        char curr = nums[0];
-6        int count = 0;
-7        
-8        
-9        for (char num : nums) {
-10            if (num == curr)
-11                count++;
-12            else {
-13                sb.append(count);
-14                sb.append(curr);
-15                curr = num;
-16                count = 1;
-17            }
-18        }
-19        
-20        
-21        sb.append(count);
-22        sb.append(curr);
-23        return sb.toString();
-24    }
-25    
-26    
-27    public String countAndSay(int n) {
-28        if (n == 1)
-29            return "1";
-30        
-31        
-32        return RLE(countAndSay(n - 1));
-33    }
-34}
+2    public String convert(String s, int numRows) {
+3        if (numRows == 1) return s;
+4        StringBuilder a = new StringBuilder();
+5        for (int i = 0; i < numRows; i++) {
+6            for (int j = i; j < s.length(); j += (2 * (numRows - 1))) {
+7                a.append(s.charAt(j));
+8                if (i > 0 && i < numRows - 1 && j + (2 * (numRows - 1)) - (2 * i) < s.length()) {
+9                    a.append(s.charAt(j + (2 * (numRows - 1)) - (2 * i)));
+10                }
+11            }
+12        }
+13        return a.toString();
+14    }
+15}
