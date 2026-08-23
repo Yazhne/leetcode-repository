d7d6338127e535b592e91764e3778acb8e428d28
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-23 21:08:53 +0530

    File updated at 8/23/2026, 9:08:52 PM

--- 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
+++ 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
@@ -1,28 +1,50 @@
-// Last updated: 8/23/2026, 8:52:17 PM
+// Last updated: 8/23/2026, 9:08:52 PM
 1class Solution {
-2    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-3        ListNode dummyHead = new ListNode(0);
-4        ListNode tail = dummyHead;
-5        int carry = 0;
-6
-7        while (l1 != null || l2 != null || carry != 0) {
-8            int digit1 = (l1 != null) ? l1.val : 0;
-9            int digit2 = (l2 != null) ? l2.val : 0;
-10
-11            int sum = digit1 + digit2 + carry;
-12            int digit = sum % 10;
-13            carry = sum / 10;
-14
-15            ListNode newNode = new ListNode(digit);
-16            tail.next = newNode;
-17            tail = tail.next;
-18
-19            l1 = (l1 != null) ? l1.next : null;
-20            l2 = (l2 != null) ? l2.next : null;
-21        }
-22
-23        ListNode result = dummyHead.next;
-24        dummyHead.next = null;
-25        return result;
-26    }
-27}
+2
+3    public void reorderList(ListNode head) {
+4
+5        if(head == null || head.next == null){
+6            return;
+7        }
+8
+9        ListNode slow = head;
+10        ListNode fast = head;
+11
+12        while(fast.next != null && fast.next.next != null){
+13            slow = slow.next;
+14            fast = fast.next.next;
+15        }
+16
+17
+18        ListNode second = slow.next;
+19        slow.next = null;
+20
+21
+22        ListNode prev = null;
+23
+24        while(second != null){
+25
+26            ListNode next = second.next;
+27
+28            second.next = prev;
+29
+30            prev = second;
+31            second = next;
+32        }
+33
+34        second = prev;
+35        ListNode first = head;
+36
+37        while(second != null){
+38
+39            ListNode temp1 = first.next;
+40            ListNode temp2 = second.next;
+41
+42            first.next = second;
+43            second.next = temp1;
+44
+45            first = temp1;
+46            second = temp2;
+47        }
+48    }
+49}
