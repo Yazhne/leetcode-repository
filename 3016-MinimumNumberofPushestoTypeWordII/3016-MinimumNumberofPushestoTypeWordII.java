c200192cd58d464450230537a2f7c518c3823579
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-23 22:50:11 +0530

    File updated at 8/23/2026, 10:50:10 PM

--- 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
+++ 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
@@ -1,40 +1,17 @@
-// Last updated: 8/23/2026, 10:48:49 PM
-1class Solution {
-2    public Node copyRandomList(Node head) {
-3        
-4        if(head == null){
-5            return head;
-6        }
-7
-8        Node temp = head;
-9        while(temp != null){
-10            Node node = new Node(temp.val);
-11            node.next = temp.next;
-12            temp.next = node;
-13            temp = temp.next.next;
-14        }
-15
-16        temp = head;
-17        while(temp != null){
-18            if(temp.random != null){
-19                temp.next.random = temp.random.next;
-20            }
-21            temp = temp.next.next;
-22        }
-23
-24        Node copyList = head.next;
-25        Node copy = copyList;
-26        Node original = head;
-27        while(original != null){
-28            original.next = original.next.next;
-29            if(copy.next != null){
-30                copy.next = copy.next.next;
-31            }
-32            original = original.next;
-33            copy = copy.next;
-34        }
-35
-36        return copyList;
-37    }
-38}
-39
+// Last updated: 8/23/2026, 10:50:10 PM
+1public class Solution {
+2    public ListNode detectCycle(ListNode head) {
+3        ListNode slow = head, fast = head;
+4        while (fast != null && fast.next != null) {
+5            slow = slow.next;
+6            fast = fast.next.next;
+7            if (slow == fast) break;
+8        }
+9        if (fast == null || fast.next == null) return null;
+10        while (head != slow) {
+11            head = head.next;
+12            slow = slow.next;
+13        }
+14        return head;
+15    }
+16}
