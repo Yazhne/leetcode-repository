388096203b3f892551336b78acbd9792b7cadf87
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-23 10:40:20 +0530

    Create 1541-TopTravellers

---
+++ 1541-TopTravellers/1541-TopTravellers.sql
@@ -0,0 +1,7 @@
+-- Last updated: 8/23/2026, 10:40:32 AM
+#Write your MySQL query statement below
+SELECT name,ifnull(Sum(Rides.distance),0) as travelled_distance 
+FROM Users
+LEFT JOIN Rides on Users.id = Rides.user_id 
+GROUP BY Rides.user_id 
+ORDER BY travelled_distance DESC, name ASC;
