fb750f1f605f31cd5b11ccafb0aa8b72631fb0de
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-23 20:37:56 +0530

    File updated at 8/23/2026, 8:37:55 PM

--- 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
+++ 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
@@ -1,43 +1,24 @@
-// Last updated: 8/23/2026, 8:35:28 PM
-1
-2
-3class RandomizedSet {
-4    private ArrayList<Integer> list;
-5    private Map<Integer, Integer> map;
-6
-7    public RandomizedSet() {
-8        list = new ArrayList<>();
-9        map = new HashMap<>();
-10    }
-11
-12    public boolean search(int val) {
-13        return map.containsKey(val);
-14    }
-15
-16    public boolean insert(int val) {
-17        if (search(val)) return false;
-18
-19        list.add(val);
-20        map.put(val, list.size() - 1);
-21        return true;
+// Last updated: 8/23/2026, 8:37:55 PM
+1class Solution {
+2    public int splitArray( int[ ] nums , int  k )  {
+3        int low = 0 , high = 0 ;
+4        for( int  i : nums ){
+5            high += i ;
+6            low = Math.max( i , low ) ;
+7        }
+8        return find( low , high , nums , k ) ;
+9    }
+10    private int find( int l , int h , int n [ ] , int k ) {
+11        if( l >h ) return l;
+12        int c = 1 , m = l + ( h - l ) / 2 , sum = 0 ;
+13        for(int i : n ) {
+14            if( i + sum > m ){
+15                sum = i ;
+16                c ++ ;
+17            }
+18            else    sum += i ;
+19        }
+20        if(c <= k ) return find( l , m - 1 , n , k ) ;
+21        return find( m + 1 , h , n , k ) ;
 22    }
-23
-24    public boolean remove(int val) {
-25        if (!search(val)) return false;
-26
-27        int index = map.get(val);
-28        list.set(index, list.get(list.size() - 1));
-29        map.put(list.get(index), index);
-30        list.remove(list.size() - 1);
-31        map.remove(val);
-32
-33        return true;
-34    }
-35
-36    public int getRandom() {
-37        Random rand = new Random();
-38        return list.get(rand.nextInt(list.size()));
-39    }
-40}
-41
-42
+23}
