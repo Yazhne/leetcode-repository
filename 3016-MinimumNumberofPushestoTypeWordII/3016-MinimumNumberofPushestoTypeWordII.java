9c36f1877f6ceada55f72008635d5d371f661e88
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-23 20:35:29 +0530

    File updated at 8/23/2026, 8:35:28 PM

--- 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
+++ 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
@@ -1,16 +1,43 @@
-// Last updated: 8/23/2026, 12:42:28 PM
-1class Solution {
-2    public boolean isValidSudoku(char[][] board) {
-3    Set seen = new HashSet();
-4    for (int i=0; i<9; ++i) {
-5        for (int j=0; j<9; ++j) {
-6            if (board[i][j] != '.') {
-7                String b = "(" + board[i][j] + ")";
-8                if (!seen.add(b + i) || !seen.add(j + b) || !seen.add(i/3 + b + j/3))
-9                    return false;
-10            }
-11        }
-12    }
-13    return true;
-14}
-15}
+// Last updated: 8/23/2026, 8:35:28 PM
+1
+2
+3class RandomizedSet {
+4    private ArrayList<Integer> list;
+5    private Map<Integer, Integer> map;
+6
+7    public RandomizedSet() {
+8        list = new ArrayList<>();
+9        map = new HashMap<>();
+10    }
+11
+12    public boolean search(int val) {
+13        return map.containsKey(val);
+14    }
+15
+16    public boolean insert(int val) {
+17        if (search(val)) return false;
+18
+19        list.add(val);
+20        map.put(val, list.size() - 1);
+21        return true;
+22    }
+23
+24    public boolean remove(int val) {
+25        if (!search(val)) return false;
+26
+27        int index = map.get(val);
+28        list.set(index, list.get(list.size() - 1));
+29        map.put(list.get(index), index);
+30        list.remove(list.size() - 1);
+31        map.remove(val);
+32
+33        return true;
+34    }
+35
+36    public int getRandom() {
+37        Random rand = new Random();
+38        return list.get(rand.nextInt(list.size()));
+39    }
+40}
+41
+42
