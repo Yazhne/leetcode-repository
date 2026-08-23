be555b7e25cdf9ebff6e6b4a01879ee210afaf06
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-23 12:36:29 +0530

    File updated at 8/23/2026, 12:36:29 PM

--- 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
+++ 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
@@ -1,16 +1,26 @@
-// Last updated: 8/23/2026, 10:42:25 AM
+// Last updated: 8/23/2026, 12:36:29 PM
 1class Solution {
-2    public String convert(String s, int numRows) {
-3        if (numRows == 1) return s;
-4        StringBuilder a = new StringBuilder();
-5        for (int i = 0; i < numRows; i++) {
-6            for (int j = i; j < s.length(); j += (2 * (numRows - 1))) {
-7                a.append(s.charAt(j));
-8                if (i > 0 && i < numRows - 1 && j + (2 * (numRows - 1)) - (2 * i) < s.length()) {
-9                    a.append(s.charAt(j + (2 * (numRows - 1)) - (2 * i)));
-10                }
-11            }
-12        }
-13        return a.toString();
-14    }
-15}
+2    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+3	Map<Integer,Integer> sums = new HashMap<>();
+4	int count = 0;
+5	for(int i=0; i<A.length;i++) {
+6		for(int j=0;j<B.length;j++){
+7			int sum = A[i]+B[j];
+8			if(sums.containsKey(sum)) {
+9				sums.put(sum, sums.get(sum)+1);
+10			} else {
+11				sums.put(sum, 1);
+12			}
+13		}
+14	}
+15	for(int k=0; k<A.length;k++) {
+16		for(int z=0;z<C.length;z++){
+17			int sum = -(C[k]+D[z]);
+18			if(sums.containsKey(sum)) {
+19				count+=sums.get(sum);
+20			}
+21		}
+22	}
+23	return count;
+24}
+25}
