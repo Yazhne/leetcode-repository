d02fd95d18bc12ee471482091d7119c6a5055d66
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-23 10:40:57 +0530

    File updated at 8/23/2026, 10:41:10 AM

--- 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
+++ 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
@@ -1,15 +1,35 @@
-// Last updated: 8/23/2026, 10:39:48 AM
+// Last updated: 8/23/2026, 10:41:10 AM
 1class Solution {
-2    public int minimumPushes(String word) {
-3        int[] freq = new int[26];
-4        for (char c : word.toCharArray()) 
-5            freq[c - 'a']++;
-6        
-7        Arrays.sort(freq);
-8        int ans = 0;
-9        for (int i = 25; i >= 0 && freq[i] > 0; i--) 
-10            ans += freq[i] * ((25 - i) / 8 + 1);
-11        
-12        return ans;
-13    }
-14}
+2    public String RLE(String sequence) {
+3        StringBuilder sb = new StringBuilder();
+4        char[] nums = sequence.toCharArray();
+5        char curr = nums[0];
+6        int count = 0;
+7        
+8        
+9        for (char num : nums) {
+10            if (num == curr)
+11                count++;
+12            else {
+13                sb.append(count);
+14                sb.append(curr);
+15                curr = num;
+16                count = 1;
+17            }
+18        }
+19        
+20        
+21        sb.append(count);
+22        sb.append(curr);
+23        return sb.toString();
+24    }
+25    
+26    
+27    public String countAndSay(int n) {
+28        if (n == 1)
+29            return "1";
+30        
+31        
+32        return RLE(countAndSay(n - 1));
+33    }
+34}
