98a86397b42bd69b63d22fdc59e6206a86e7be43
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-23 22:52:08 +0530

    File updated at 8/23/2026, 10:52:08 PM

--- 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
+++ 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
@@ -1,74 +1,26 @@
-// Last updated: 8/23/2026, 10:51:13 PM
-1class LRUCache {
-2    class Node {
-3        int key;
-4        int val;
-5        Node prev;
-6        Node next;
-7
-8        Node(int key, int val) {
-9            this.key = key;
-10            this.val = val;
-11        }
-12    }
-13
-14    Node head = new Node(-1, -1);
-15    Node tail = new Node(-1, -1);
-16    int cap;
-17    HashMap<Integer, Node> m = new HashMap<>();
-18
-19    public LRUCache(int capacity) {
-20        cap = capacity;
-21        head.next = tail;
-22        tail.prev = head;
-23    }
-24
-25    private void addNode(Node newnode) {
-26        Node temp = head.next;
-27
-28        newnode.next = temp;
-29        newnode.prev = head;
-30
-31        head.next = newnode;
-32        temp.prev = newnode;
-33    }
-34
-35    private void deleteNode(Node delnode) {
-36        Node prevv = delnode.prev;
-37        Node nextt = delnode.next;
-38
-39        prevv.next = nextt;
-40        nextt.prev = prevv;
-41    }
-42
-43    public int get(int key) {
-44        if (m.containsKey(key)) {
-45            Node resNode = m.get(key);
-46            int ans = resNode.val;
-47
-48            m.remove(key);
-49            deleteNode(resNode);
-50            addNode(resNode);
-51
-52            m.put(key, head.next);
-53            return ans;
-54        }
-55        return -1;
-56    }
-57
-58    public void put(int key, int value) {
-59        if (m.containsKey(key)) {
-60            Node curr = m.get(key);
-61            m.remove(key);
-62            deleteNode(curr);
-63        }
-64
-65        if (m.size() == cap) {
-66            m.remove(tail.prev.key);
-67            deleteNode(tail.prev);
-68        }
-69
-70        addNode(new Node(key, value));
-71        m.put(key, head.next);
-72    }
-73}
+// Last updated: 8/23/2026, 10:52:08 PM
+1class MinStack {
+2    int min = Integer.MAX_VALUE;
+3    Stack<Integer> stack = new Stack<Integer>();
+4    public void push(int x) {
+5      
+6        if(x <= min){          
+7            stack.push(min);
+8            min=x;
+9        }
+10        stack.push(x);
+11    }
+12
+13    public void pop() {
+14        
+15        if(stack.pop() == min) min=stack.pop();
+16    }
+17
+18    public int top() {
+19        return stack.peek();
+20    }
+21
+22    public int getMin() {
+23        return min;
+24    }
+25}
