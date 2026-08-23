1ef053086a13887849f73f004b551dd2be1328cd
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-23 10:41:34 +0530

    Create 693-BinaryNumberWithAlternatingBits

---
+++ 693-BinaryNumberWithAlternatingBits/693-BinaryNumberWithAlternatingBits.java
@@ -0,0 +1,14 @@
+// Last updated: 8/23/2026, 10:41:46 AM
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        String binary = Integer.toBinaryString(n);
+        char[] arr = binary.toCharArray();
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == arr[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
