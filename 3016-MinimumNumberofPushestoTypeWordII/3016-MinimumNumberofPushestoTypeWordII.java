f277fbe1724b6068e150c35d2712ff298153fbbe
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-23 22:47:05 +0530

    File updated at 8/23/2026, 10:47:04 PM

--- 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
+++ 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
@@ -1,25 +1,62 @@
-// Last updated: 8/23/2026, 10:46:08 PM
+// Last updated: 8/23/2026, 10:47:04 PM
 1class Solution {
-2    public ListNode deleteDuplicates(ListNode head) {
-3        if (head == null || head.next == null) return head;
-4
-5        ListNode dummy = new ListNode(-1); 
-6        dummy.next = head;
-7        ListNode prev = dummy;
-8        ListNode cur = head;
-9
-10        while (cur != null && cur.next != null) {
-11            if (cur.val == cur.next.val) {
-12                while (cur.next != null && cur.val == cur.next.val) {
-13                    cur = cur.next;
-14                }
-15                prev.next = cur.next; 
-16            } else {
-17                prev = prev.next; 
-18            }
-19            cur = cur.next;
-20        }
-21
-22        return dummy.next;
-23    }
-24}
+2    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+3        double ans=0;
+4        int count=-1;
+5        int n=nums1.length;
+6        int m=nums2.length;
+7        int len=(n+m);
+8        
+9        double x=0;
+10        double y=0;
+11        int i=0;
+12        int j=0;
+13        while(i<n&&j<m){
+14            if(nums1[i]<nums2[j]){
+15                count++;
+16                if(count==(len-1)/2){
+17                    x=nums1[i];
+18                }
+19                else if(count==(len/2)){
+20                    y=nums1[i];
+21                }
+22                i++;
+23            }
+24            else {
+25                count++;
+26                if(count==(len-1)/2){
+27                    x=nums2[j];
+28                }
+29                else if(count==(len/2)){
+30                    y=nums2[j];
+31                }
+32             j++;
+33            }
+34        }
+35        while(i<n){
+36            count++;
+37            if(count==(len-1)/2){
+38                x=nums1[i];
+39            }
+40            else if(count==len/2){
+41                y=nums1[i];
+42            }
+43            i++;
+44        }
+45
+46         while(j<m){
+47            count++;
+48            if(count==(len-1)/2){
+49                x=nums2[j];
+50            }
+51            else if(count==len/2){
+52                y=nums2[j];
+53            }
+54            j++;
+55        }
+56        if(len%2==1) return x;
+57        return (x+y)/2;
+58
+59
+60    }
+61}
