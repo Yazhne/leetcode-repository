3a6920290a47bd701bcaca5b4d6b199c172dc736
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-23 22:53:04 +0530

    File updated at 8/23/2026, 10:53:03 PM

--- 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
+++ 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
@@ -1,26 +1,35 @@
-// Last updated: 8/23/2026, 10:52:08 PM
-1class MinStack {
-2    int min = Integer.MAX_VALUE;
-3    Stack<Integer> stack = new Stack<Integer>();
-4    public void push(int x) {
-5      
-6        if(x <= min){          
-7            stack.push(min);
-8            min=x;
-9        }
-10        stack.push(x);
-11    }
-12
-13    public void pop() {
-14        
-15        if(stack.pop() == min) min=stack.pop();
-16    }
-17
-18    public int top() {
-19        return stack.peek();
-20    }
-21
-22    public int getMin() {
-23        return min;
-24    }
-25}
+// Last updated: 8/23/2026, 10:53:03 PM
+1class Solution {
+2    public String minWindow(String s, String t) {
+3        if (s == null || t == null || s.length() == 0 || t.length() == 0 ||
+4                s.length() < t.length()) {
+5            return new String();
+6        }
+7        int[] map = new int[128];
+8        int count = t.length();
+9        int start = 0, end = 0, minLen = Integer.MAX_VALUE, startIndex = 0;
+10        for (char c : t.toCharArray()) {
+11            map[c]++;
+12        }
+13
+14        char[] chS = s.toCharArray();
+15
+16        while (end < chS.length) {
+17            if (map[chS[end++]]-- > 0) {
+18                count--;
+19            }
+20            while (count == 0) {
+21                if (end - start < minLen) {
+22                    startIndex = start;
+23                    minLen = end - start;
+24                }
+25                if (map[chS[start++]]++ == 0) {
+26                    count++;
+27                }
+28            }
+29        }
+30
+31        return minLen == Integer.MAX_VALUE ? new String() :
+32                new String(chS, startIndex, minLen);
+33    }
+34}
