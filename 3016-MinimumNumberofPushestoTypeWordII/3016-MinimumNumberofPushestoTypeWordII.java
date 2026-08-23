a5c01721dad153d79332e3d2115b084ab6766f1a
YAZHNE M N <yazhnemn9007@gmail.com>
2026-08-23 22:58:39 +0530

    File updated at 8/23/2026, 10:58:38 PM

--- 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
+++ 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
@@ -1,30 +1,34 @@
-// Last updated: 8/23/2026, 10:56:59 PM
-1class Solution {
-2    public ListNode partition(ListNode head, int x) {
-3        if(head == null || head.next == null) {
-4            return head;
-5        }
-6        ListNode less = new ListNode(-1);
-7        ListNode more = new ListNode(-1);
-8
-9        ListNode tail1 = less;
-10        ListNode tail2 = more;
-11
-12        ListNode temp = head;
-13        while(temp != null) {
-14            if(temp.val < x) {
-15                tail1.next = temp;
-16                tail1 = tail1.next;
-17            }
-18            else {
-19                tail2.next = temp;
-20                tail2 = tail2.next;
-21            }
-22            temp = temp.next;
-23        }
-24        
-25        tail1.next = more.next;
-26        tail2.next = null;
-27        return less.next;
-28    }
-29}
+// Last updated: 8/23/2026, 10:58:38 PM
+1
+2class Solution {
+3    public int sumDistance(int[] nums, String s, int d) {
+4        
+5        int n = nums.length;
+6        long temp[] = new long[n];
+7        for (int i = 0; i < s.length(); i++) {
+8            if(s.charAt(i) == 'R'){
+9                temp[i] = (long)nums[i] + d;
+10            }else{
+11                temp[i] = (long)nums[i] - d;
+12            }
+13        }
+14
+15        Arrays.sort(temp);
+16
+17        long res = 0;
+18        int j = n - 1;
+19
+20        for (int i = 0; i < n; i++) {
+21            if (j - i <= 0) {
+22                break;
+23            }
+24            long diff = Math.abs(temp[j] - temp[i]);
+25            long prod = diff * (j - i);
+26            res += prod;
+27            res %= 1000000007;
+28            j--;
+29        }
+30
+31        return (int) res;
+32    }
+33}
